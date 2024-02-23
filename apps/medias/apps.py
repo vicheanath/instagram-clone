11636e1705be4826056b5488be09c88f59e6b89b
Vichea <48352653+vicheanath@users.noreply.github.com>
2024-02-23 11:41:56 -0600

    feat: create custom UserModel and added django grapql-auth

--- apps/medias/apps.py
+++ apps/medias/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MediasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medias'
+    name = 'apps.medias'
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.apps import apps
 
-# Register your models here.
+from apps.users.models import User
+
+app = apps.get_app_config('graphql_auth')
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
+
+
+# UserAdmin class with password change and reset
+class UserAdmin(UserAdmin):
+    list_display = ["email", "name", "username", "is_superuser"]
+    search_fields = ["email", "name", "username"]
+    list_filter = ["is_superuser", "is_active"]
+    ordering = ["email"]
+    filter_horizontal = []
+    fieldsets = []
+    add_fieldsets = []
+    readonly_fields = ["created_at", "updated_at"]
+    actions = []
+
+    def get_fieldsets(self, request, obj=None):
+        return (
+            (None, {"fields": ("email", "password")}),
+            ("Personal info", {"fields": ("name", "username", "bio", "website", "location", "birth_date")}),
+            ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+            ("Important dates", {"fields": ("last_login", "created_at", "updated_at")}),
+        )
+
+    def get_add_fieldsets(self, request, obj=None):
+        return (
+            (None, {"fields": ("email", "password1", "password2")}),
+            ("Personal info", {"fields": ("name", "username", "bio", "website", "location", "birth_date")}),
+            ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        )
+
+
+admin.site.register(User, UserAdmin)
